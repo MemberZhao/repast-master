f1ef628be52f6d8895e78701eca712b021ca0130
861883901@qq.com <zhy.15188342636>
2019-12-04 16:14:32 +0800

    二期项目后台模块

---
+++ repast-common/src/main/java/com/aaa/repast/common/exception/DemoModeException.java
@@ -0,0 +1,15 @@
+package com.aaa.repast.common.exception;
+
+/**
+ * 演示模式异常
+ * 
+ * @author Seven Lee
+ */
+public class DemoModeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException()
+    {
+    }
+}
